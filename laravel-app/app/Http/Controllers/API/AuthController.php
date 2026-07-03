528973168a0d4e96f56a9bea5245fa1858bfad43
unknown <pensreyneat@gmail.com>
2026-07-03 09:59:50 +0700

    user resource

--- laravel-app/app/Http/Controllers/API/AuthController.php
+++ laravel-app/app/Http/Controllers/API/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\SigninRequest;
+use App\Http\Requests\User\SignupRequest;
+use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -10,15 +13,8 @@ use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    function signup(Request $request)
+    function signup(SignupRequest $request)
     {
-
-        $request->validate([
-            'name' => 'required|string|max:100',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:6|max:10|confirmed'
-        ]);
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -27,19 +23,14 @@ class AuthController extends Controller
 
         return response([
             'message' => 'User signed up.',
-            'user' => $user
+            'user' => new UserResource($user)
         ], 201);
     }
 
-    function signin(Request $request)
+    function signin(SigninRequest $request)
     {
-        $request->validate([
-            'email' => 'required|email|exists:users,email',
-            'password' => 'required|string|min:6|max:10'
-        ]);
-
         $user = User::where('email', $request->email)->first();
-        // select * from users where email ="pensreyneat@gmail.com"
+
         if (!Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'password' => 'Password does not match.',
@@ -50,7 +41,7 @@ class AuthController extends Controller
 
         return response([
             'message' => 'User signed in.',
-            'user' => $user,
+            'user' => new UserResource($user),
             'token' => $token
         ], 200);
     }
@@ -63,8 +54,8 @@ class AuthController extends Controller
         $user->currentAccessToken()->delete();
 
         // option 2
-        // $currentToken = $user->currentAccessToken();
-        // $user->tokens()->where('id', $currentToken->id)->delete();
+        $currentToken = $user->currentAccessToken();
+        $user->tokens()->where('id', $currentToken->id)->delete();
 
         return response([
             'message' => 'User signed out.'
@@ -75,7 +66,7 @@ class AuthController extends Controller
     {
         return response([
             'message' => 'Token is valid.',
-            'user' => $request->user()
+            'user' => new UserResource($request->user())
         ], 200);
     }
 }
